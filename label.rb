2cdc8ed43545998341d4ce447484f7ff99f30372
Jesus Molina <netod90@gmail.com>
2023-07-18 09:37:32 -0700

    Add item class, implement item methods, and main.rb

---
+++ label.rb
@@ -0,0 +1,15 @@
+class Label
+  attr_reader :id, :title, :color
+  attr_accessor :items
+
+  def initialize(id, title, color)
+    @id = id
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+  end
+end
