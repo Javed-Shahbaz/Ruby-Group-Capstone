552cc5971a4ad8033b7e5417e676f4dc3304c777
abiola <abiola.arobieke@gmail.com>
2023-07-19 11:11:37 -0400

    Add test for game.rb file

---
+++ tests/game_spec.rb
@@ -0,0 +1,34 @@
+require './item'
+require './app_methods/game'
+require 'date'
+​
+describe Game do
+  let(:game) { Game.new(true, last_played_at, '2005-12-28') }
+  let(:last_played_at) { '2011-06-30' }
+  ​
+  describe '#can_be_archived?' do
+    context 'when the item cannot be archived' do
+      let(:item) { double('item') }
+      ​
+      before do
+        allow(game).to receive(:super)
+      end
+      ​
+      it 'returns false' do
+        expect(game.can_be_archived?).to be(true)
+      end
+    end
+    ​
+    context 'when the item can be archived' do
+      let(:item) { double('item') }
+      ​
+      before do
+        allow(game).to receive(:super)
+      end
+      ​
+      it 'returns true' do
+        expect(game.can_be_archived?).to be(true)
+      end
+    end
+  end
+end
