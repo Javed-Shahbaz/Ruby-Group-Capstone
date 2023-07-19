cefb07bd804382bbccabf15f5001cebbc7ca355e
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-07-19 15:51:16 +0500

    update item

--- item.rb
+++ item.rb
@@ -1,32 +1,48 @@
-require_relative 'genre'
-require_relative 'author'
-require_relative 'source'
-require_relative 'label'
+require 'date'
+require_relative 'app_methods/label'
 
 class Item
-  attr_reader :id, :genre, :author, :source, :label, :publish_date
-  attr_accessor :archived
+  attr_accessor :id, :author, :genre, :source, :label, :publish_date, :archived
 
-  def initialize(genre, author, source, label, publish_date)
+  def initialize(publish_date)
     @id = Random.rand(1..1000)
-    @genre = genre
-    @author = author
-    @source = source
-    @label = label
     @publish_date = publish_date
     @archived = false
   end
 
   def can_be_archived?
-    (Time.now - @publish_date) >= (10 * 365 * 24 * 60 * 60) # ten years (seconds)
+    date = Date.new(@publish_date.to_i)
+    (Date.today - date).to_i > 3650
   end
 
   def move_to_archive
-    if can_be_archived?
+    if can_be_archived? == true
       @archived = true
-      puts 'Item has been archived.'
     else
-      puts 'Item cannot be archived yet.'
+      puts 'This item cannot be archived'
     end
+  end
+
+  def genre=(genre)
+    if genre.is_a?(Genre)
+      @genre = genre
+      genre.items << self
+    else
+      puts 'Invalid genre'
+    end
+  end
+
+  def label=(label)
+    if label.is_a?(Label)
+      @label = label
+      label.items << self
+    else
+      puts 'Invalid label'
+    end
+  end
+
+  def author=(author)
+    @author = author
+    author.items.push(self) unless author.items.include?(self)
   end
 end
